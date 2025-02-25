26ee4d6fd0d6e3a9a951fda8bfff9bf46d8aae16
Marcos Bergami <56553101+marcosbergami@users.noreply.github.com>
2025-02-25 17:01:06 -0600

    feat: remove deprecated _document.tsx and update layout to include favicon link

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/public/favicon.svg" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
